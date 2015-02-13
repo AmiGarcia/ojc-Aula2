76fe82beda65902c136df28cc94e3a3a6ac65301
Ami Garcia <AmiGarcia@Amis-MacBook-Pro.local>
2015-02-13 14:20:07 -0200

    Initial Commit

---
+++ ojc-Aula2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ojc-Aula2
+//
+//  Created by Ami Garcia on 2/13/15.
+//  Copyright (c) 2015 Ami Garcia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
